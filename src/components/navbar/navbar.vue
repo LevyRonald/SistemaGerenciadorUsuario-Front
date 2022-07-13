c97f45b9a3203e8e331fdcef2aa1f826303bd68f
Levy <levyronald3@gmail.com>
2022-07-13 14:43:37 -0300

    feat: adicionando navbar da pagina

---
+++ src/components/navbar/navbar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+    <b-navbar type="dark"  class="navbar">
+      <b-navbar-nav class="d-flex justify-content-between w-100">
+        <nav class="d-flex align-items-center p-0">
+          <b-nav-item to="inicio">ADMIN</b-nav-item>
+        </nav>
+        <div class="d-flex align-items-center">
+          <b-nav-item to="inicio">home</b-nav-item>
+          <b-nav-item>cadastrar</b-nav-item>
+          <b-nav-item to="tabela">usuários</b-nav-item>
+          <b-nav-item-dropdown right>
+            <template #button-content>
+              <b-icon icon="people-fill" aria-hidden="true"></b-icon>
+            </template>
+            <b-dropdown-item>Conta</b-dropdown-item>
+            <b-dropdown-item @click.prevent="Logout">Sair</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </div>
+      </b-navbar-nav>
+    </b-navbar>
+  </div>
+</template>
+<script>
+export default {
+  methods: {
+    Logout() {
+      localStorage.removeItem("token");
+      this.$router.push({ name: "login" });
+    },
+  },
+};
+</script>
+<style scoped>
+.navbar{
+    background-color: #140F2D;
+}
+</style>

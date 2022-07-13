9622cc635acc3585fa7725d6946e58a71c7e090c
Levy <levyronald3@gmail.com>
2022-07-13 14:44:47 -0300

    feat: reorganizando home

---
+++ src/views/home/home.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="fundo">
+    <navbar />
+    <div class="d-flex align-items-center justify-content-center pt-4">
+      <router-view/>
+    </div>
+  </div>
+</template>
+<script>
+import navbar from "@/components/navbar/navbar.vue";
+import users from "@/views/tabela/users.vue";
+import inicio from "@/components/inicio/inicio.vue"
+
+export default {
+  components: {
+    navbar,
+    users,
+    inicio
+  },
+};
+</script>
+<style scoped>
+.fundo {
+  background-color: #5b635b8f;
+  height: 100vh;
+}
+</style>

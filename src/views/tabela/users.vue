84ab67dd4cb8f9410c33eef4b1ebe7425b0aaaae
Levy <levyronald3@gmail.com>
2022-07-20 16:33:37 -0300

    feat: Implementando ações para editar usuario

--- src/views/tabela/users.vue
+++ src/views/tabela/users.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <b-card no-body class="mb-2 cardPrin">
+    <b-card no-body class="mb-2 cardprin shadow" v-dark-mode>
       <div class="m-2">
         <b-row>
           <b-col
@@ -27,8 +27,9 @@
             >
               <b-form-input
                 v-model="filter"
-                class="d-inline-block mr-1 h-75 w-50"
+                class="d-inline-block mr-1 h-75 w-50 input"
                 placeholder="Pesquisar..."
+                v-dark-mode
               />
             </div>
           </b-col>
@@ -38,38 +39,64 @@
         <Tabela v-for="user in users" :key="user.numero" :user="user" />
       </div>
       <b-table
-        fixed
         striped
         hover
         responsive
+        :dark="switchMode"
         :items="users"
         :fields="column"
         :filter="filter"
         :filter-included-fields="filterOn"
         @filtered="onFiltered"
-         :per-page="perPage"
-        :current-page="currentPage"
+        sticky-header="470px"
+        no-border-collapse
         show-empty
       >
-        <template #cell(name)="data">{{ data.value }}</template>
+        <template #cell(actions)="{ item }">
+          <b-dropdown no-caret variant="flat">
+            <template #button-content>
+              <b-icon
+                icon="three-dots-vertical"
+                :variant="switchIcon"
+                class="align-middle"
+              ></b-icon>
+            </template>
+            <b-dropdown-item :to="{ path: `/home/editar/${item._id}` }">
+              <b-icon icon="box-arrow-up-right" scale="0.6"></b-icon>
+              <label for="">Editar</label>
+            </b-dropdown-item>
+            <b-dropdown-item>
+              <b-icon icon="trash" scale="0.6"></b-icon>
+              <label for="">Excluir</label>
+            </b-dropdown-item>
+          </b-dropdown>
+        </template>
       </b-table>
-      <b-pagination
-        v-model="currentPage"
-        :total-rows="totalRows"
-        :per-page="perPage"
-        first-number
-        last-number
-        class="mb-0 mt-1 mt-sm-0"
-        prev-class="prev-item"
-        next-class="next-item"
+      <b-modal
+        id="modal-center"
+        title="Editar usuário"
+        ok-title="Atualizar"
+        cancel-variant="outline-secondary"
+        cancel-title="fechar"
+        centered
+        :header-bg-variant="switchModal"
+        :body-bg-variant="switchModal"
+        :footer-bg-variant="switchModal"
+        content-class="shadow"
       >
-        <template #prev-text>
-          <b-icon-arrow-left-circle/>
-        </template>
-        <template #next-text>
-          <b-icon-arrow-right-circle/>
-        </template>
-      </b-pagination>
+        <b-form @submit.prevent="update">
+          <b-form-group label="Nome" label-for="Nome">
+            <b-form-input class="input shadow-none"  v-model="editable.name" v-dark-mode></b-form-input>
+          </b-form-group>
+          <b-form-group label="Email" label-for="Email  ">
+            <b-form-input class="input shadow-none" type="email"  v-model="editable.email"  v-dark-mode></b-form-input>
+          </b-form-group>
+          <b-form-group label="Senha" label-for="Senha">
+            <b-form-input class="input shadow-none" type="password" v-model="editable.password"  v-dark-mode></b-form-input>
+          </b-form-group>
+          <b-button type="submit">atualizar</b-button>
+        </b-form>
+      </b-modal>
     </b-card>
   </div>
 </template>
@@ -84,7 +111,15 @@ export default {
   },
   data() {
     return {
+      stickyHeader: true,
+      noCollapse: false,
       users: [],
+      editusers: {
+        name: "",
+        email: "",
+        password: "",
+      },
+      editable: {},
       perPage: 5,
       pageOptions: [3, 5, 10],
       totalRows: 1,
@@ -99,7 +134,11 @@ export default {
         },
         {
           key: "email",
-          label: "email",
+          label: "Email",
+        },
+        {
+          key: "actions",
+          label: "Ações",
         },
       ],
     };
@@ -115,11 +154,43 @@ export default {
       this.totalRows = filteredItems.length;
       this.currentPage = 1;
     },
+    getUsers(users){
+      this.editable = {
+        ...users
+      }
+      this.$bvModal.show('modal-center')
+    },
+    // update({id}){
+    //   this.$http
+    //     .patch(`/users/update/${id}`, this.editusers)
+    //     .then((response) => {
+    //       console.log(response);
+    //       this.$router.push({ name: "tabela" });
+    //     })
+    //     .catch((erro) => console.log(erro));
+    // },
+  },
+  computed: {
+    switchMode() {
+      return Boolean(this.$store.state.darkMode);
+    },
+    switchIcon() {
+      return this.switchMode ? "light" : "dark";
+    },
+    switchModal(){
+      return this.switchMode ? "dark" : "light";
+    }
   },
 };
 </script>
 <style scoped>
-.cardPrin {
-  background-color: #cad5ca;
+.cardprin.theme--dark {
+  background-color: #313336;
+}
+
+.input.theme--dark {
+  background-color: #17181a;
+  color: white;
+  border-color: #4a4d53;
 }
 </style>

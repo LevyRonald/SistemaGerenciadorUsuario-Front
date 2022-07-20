2d0484916ea2d1bd41dff8cf48e27b365b28cca4
Levy <levyronald3@gmail.com>
2022-07-20 08:31:45 -0300

    refactor: refatorando para darkmode

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
 <template>
-  <div>
-    <router-view/>
-  </div>
+  <body id="app" v-dark-mode>
+    <div>
+      <router-view />
+    </div>
+  </body>
 </template>
+<script>
+</script>
 
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
 }
-
+#app.theme--dark {
+  color: white;
+}
+body.theme--dark {
+  background-color: #0d1117;
+}
 nav {
   padding: 30px;
 }

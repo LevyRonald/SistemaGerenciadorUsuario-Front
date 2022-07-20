7e82d00a6403f41f8b6d45636b1e64fc433de126
Levy <levyronald3@gmail.com>
2022-07-20 08:21:53 -0300

    feat: implememtando dark mode

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import provedor from './provedor'
 import store from './store'
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
-
+import { validate } from 'vee-validate'
+import VueApexCharts from 'vue-apexcharts'
 import http from '@/http'
+import "@/directives/index";
+
 
 Vue.config.productionTip = false
 
@@ -14,9 +18,15 @@ Vue.prototype.$http = http
 
 Vue.use(BootstrapVue)
 Vue.use(IconsPlugin)
+Vue.use(validate)
+Vue.use(VueApexCharts)
+
+
+Vue.component('apexchart', VueApexCharts)
+
 
 new Vue({
   router,
-  store,
+  store: provedor,
   render: h => h(App)
 }).$mount('#app')

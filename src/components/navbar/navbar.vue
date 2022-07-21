9eaed48138ad7ab64573600c8a87fabc40eee20a
Levy <levyronald3@gmail.com>
2022-07-21 14:42:20 -0300

    feat: exibindo nome de usuário logado na navbar

--- src/components/navbar/navbar.vue
+++ src/components/navbar/navbar.vue
@@ -23,18 +23,29 @@
             ><p class="navbarTest m-0" v-dark-mode>Cadastrar</p></b-nav-item
           >
           <b-nav-item to="tabela"
-            ><p class="navbarTest p-0 m-0" v-dark-mode>Usuários</p></b-nav-item
+            ><p class="navbarTest p-0 m-0" v-dark-mode>usuarios</p></b-nav-item
           >
-          <b-nav-item-dropdown right>
+          <b-nav-item-dropdown
+            toggle-class="d-flex align-items-center dropdown-user-link"
+            right
+           >
             <template #button-content>
+              <div class="d-sm-flex d-none user-nav">
+                <p class="user-name font-weight-bolder mb-0" v-dark-mode>
+                  {{user.name}}
+                </p>
+                <span class="user-status" v-dark-mode>Admin</span>
+              </div>
               <b-avatar
+                variant="light-primary"
                 badge
+                class="badge-minimal"
                 badge-variant="success"
-                size="30px"
+                size="40"
                 src="https://placekitten.com/300/300"
               ></b-avatar>
             </template>
-            <b-dropdown-item>Conta</b-dropdown-item>
+            <b-dropdown-item >Conta</b-dropdown-item>
             <b-dropdown-item @click.prevent="Logout">Sair</b-dropdown-item>
           </b-nav-item-dropdown>
         </div>
@@ -43,7 +54,19 @@
   </div>
 </template>
 <script>
+import provedor from '@/provedor';
 export default {
+  data(){
+    return{
+      user:{
+        name: '',
+        email: ''
+      }
+    }
+  },
+  mounted(){
+    this.user = provedor.state.usuario
+  },
   methods: {
     Logout() {
       this.$store.commit("DESLOGAR_USUARIO");
@@ -69,5 +92,21 @@ export default {
 }
 .navbar.theme--dark {
   background-color: #313336;
+}
+.user-name.theme--dark{
+  color: rgb(255, 255, 255) !important;
+}
+.user-status.theme--dark{
+  color: rgb(255, 255, 255) !important;
+}
+ .user-nav {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-end;
+  float: left;
+  margin-right: 4px;
+}
+.user-status {  
+  font-size: 12px;
 }
 </style>

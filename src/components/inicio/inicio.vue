6a0855bb2744d11afa53ffc637c15083a87e3ac1
Levy <levyronald3@gmail.com>
2022-08-02 16:42:00 -0300

    refactor: reorganizando página inicial

--- src/components/inicio/inicio.vue
+++ src/components/inicio/inicio.vue
@@ -1,53 +1,90 @@
 <template>
-  <b-card class="shadow" v-dark-mode>
-    <label for="">ADMIN - taxa de crescimento</label>
-    <div>
-      <apexchart
-      class="apex-style"
-        type="line"
-        :options="chartOptions"
-        :series="series"
-        v-dark-mode
-      ></apexchart>
-    </div>
-  </b-card>
+  <b-row class="principal">
+    <b-col class="pb-3">
+      <b-card class="shadow" v-dark-mode>
+        <label for="">ADMIN - taxa de crescimento</label>
+        <apexchart
+          class="apex-style"
+          type="line"
+          :options="salesBar.chartOptions"
+          :series="salesBar.series"
+          v-dark-mode
+        ></apexchart>
+      </b-card>
+    </b-col>
+    <b-col>
+      <b-card class="shadow" v-dark-mode>
+        <label for="">ADMIN - taxa de usuários</label>
+        <apexchart
+          class="apex-style"
+          type="donut"
+          :options="Donut.chartOptions"
+          :series="Donut.series"
+          v-dark-mode
+        ></apexchart>
+      </b-card>
+    </b-col>
+  </b-row>
 </template>
 <script>
 export default {
   data: function () {
     return {
-      chartOptions: {
-        chart: {
-          id: "vuechart-example",
+      salesBar: {
+        chartOptions: {
+          chart: {
+            id: "vuechart-example",
+          },
+          xaxis: {
+            categories: [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2021],
+          },
         },
-        xaxis: {
-          categories: [2015, 2016, 2017, 2018, 2019, 2020, 2021, 2021],
+        series: [
+          {
+            name: "taxa de crescimento",
+            data: [30, 40, 35, 50, 49, 60, 70, 91],
+          },
+        ],
+      },
+      Donut: {
+        series: [44, 55],
+        chartOptions: {
+          chart: {
+            type: "donut",
+          },
+          labels: ["admin", "usuário"],
+          // responsive: [
+          //   {
+          //     breakpoint: 480,
+          //     options: {
+          //       chart: {
+          //         width: 200,
+          //       },
+          //       legend: {
+          //         position: "bottom",
+          //       },
+          //     },
+          //   },
+          // ],
         },
       },
-      series: [
-        {
-          name: "taxa de crescimento",
-          data: [30, 40, 35, 50, 49, 60, 70, 91],
-        },
-      ],
-      
     };
   },
 };
 </script>
 <style scoped>
-.shadow.theme--dark{
+.shadow.theme--dark {
   background-color: #313336;
 }
-.apex-style{
-  width: 600px;
+.apex-style {
+  width: 500px;
 }
-@media screen and (max-width: 800px){
-  .apex-style{
+@media screen and (max-width: 800px) {
+  .principal{
     width: 100%;
   }
-  .shadow{
-    width: 100%;
+  .apex-style{
+    width: 300px;
   }
 }
 </style>
